1ebd8830ac4c6e14d98d8da2d4a334136b3f9a9d
Zeus <the1zeus@gmail.com>
2017-11-28 16:34:01 -0600

    Include script overhaul

--- includes/core.js
+++ includes/core.js
@@ -1,36 +1,43 @@
-function require(name) {
-	includes[name].required = true;
-}
-
-function addInclude(path) {
-	if (!(~path.indexOf(':')) && path[0] !== ".") {
-		path = 'http://www.unclejasonteachesprogramming.com/gitjs/p5-sketches/master/' + path;
-	}
-	var script = document.createElement("script");
-	script.src = path;
-	document.head.appendChild(script);
-}
-
-var p5Version = '0.5.15';
-var includes = {
-	"shapes": {
-		"path": "includes/shapes.js",
-		"description": "Creates functions for generating simple but not trivial geometric shapes, such as stars and hearts.",
-		"required": false
-	},
-	"particle": {
-		"path": "includes/Particle.js",
-		"description": "Custom simple UJTP particle class.",
-		"required": false
-	},
-	"p5.play": {
-		"path": "https://cdn.rawgit.com/molleindustria/p5.play/master/lib/p5.play.js",
-		"description": "p5.play provides sprites, animations, input and collision functions for games and gamelike applications. Created by Paolo Pedercini.",
-		"required": "false"
-	}
-};
-
 (function () {
+	var extraIncludes = [];
+	var init = false;
+	var inc = {
+		p5Version: '0.5.15',
+		require: function (name) {
+			inc.includes[name].required = true;
+		},
+		addInclude: function (path) {
+			if (!init) {
+				extraIncludes.push(path);
+				return;
+			}
+			if (!(~path.indexOf(':')) && path[0] !== ".") {
+				path = 'http://www.unclejasonteachesprogramming.com/gitjs/p5-sketches/master/' + path;
+			}
+			var script = document.createElement("script");
+			script.src = path;
+			document.head.appendChild(script);
+		},
+		includes: {
+			"shapes": {
+				"path": "includes/shapes.js",
+				"description": "Creates functions for generating simple but not trivial geometric shapes, such as stars and hearts.",
+				"required": false
+			},
+			"particle": {
+				"path": "includes/Particle.js",
+				"description": "Custom simple UJTP particle class.",
+				"required": false
+			},
+			"p5.play": {
+				"path": "https://cdn.rawgit.com/molleindustria/p5.play/master/lib/p5.play.js",
+				"description": "p5.play provides sprites, animations, input and collision functions for games and gamelike applications. Created by Paolo Pedercini.",
+				"required": "false"
+			}
+		}
+	};
+
+
 	function addEvent(element, eventName, fn) {
 		if (element.addEventListener) {
 			element.addEventListener(eventName, fn, false);
@@ -40,17 +47,21 @@ var includes = {
 	}
 	addEvent(window, 'load', function () {
 		if (window.preInit && typeof window.preInit == 'function') {
-			window.preInit();
+			window.preInit(inc);
 		}
-		addInclude('https://cdnjs.cloudflare.com/ajax/libs/p5.js/' + p5Version + '/p5.min.js');
-		for (var lib in includes) {
-			if (includes[lib].required) {
-				if (typeof includes[lib].path == 'function') {
-					addInclude(includes[lib].path());
+		init = true;
+		inc.addInclude('https://cdnjs.cloudflare.com/ajax/libs/p5.js/' + inc.p5Version + '/p5.min.js');
+		for (var lib in inc.includes) {
+			if (inc.includes[lib].required) {
+				if (typeof inc.includes[lib].path == 'function') {
+					inc.addInclude(inc.includes[lib].path());
 				} else {
-					addInclude(includes[lib].path);
+					inc.addInclude(inc.includes[lib].path);
 				}
 			}
+		}
+		for (var i = 0; i < extraIncludes.length; i++) {
+			inc.addInclude(extraIncludes[i]);
 		}
 	});
 }());
